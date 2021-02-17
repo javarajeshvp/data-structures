8f30cd952197adaf3da425db39e83996f8ed1bc6
javarajeshvp <javarajeshvp@gmail.com>
2021-02-17 10:01:35 -0700

    Update Node.java

--- Code/List/src/com/vp/ds/linkedlist/Node.java
+++ Code/List/src/com/vp/ds/linkedlist/Node.java
@@ -1,28 +1,49 @@
 package com.vp.ds.linkedlist;
 
-public class Node {
-	
-	private Node next = null;
-	private Object data=null;
+import java.io.Serializable;
 
-	public Node(Object data)
+public class Node implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Object data;
+	Node next;
+	Node prev;
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node(int data)
 	{
 		this.data = data;
 	}
-	public Node getNext()
-	{
-		return next;
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + ", next=" + next + ", prev=" + prev + "]";
 	}
-	public Object getData()
-	{
+
+	public Object getData() {
 		return data;
 	}
-	public void setNext(Node next)
-	{
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
 		this.next = next;
 	}
-	public void setData(Object data)
-	{
-		this.data=data;
-	}
+
 }
