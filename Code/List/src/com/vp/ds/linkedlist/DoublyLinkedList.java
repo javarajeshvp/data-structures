0d4fc2717889216e212ac204a0649205629ce16a
javarajeshvp <javarajeshvp@gmail.com>
2021-02-19 15:15:35 -0700

    Update DoublyLinkedList.java

--- Code/List/src/com/vp/ds/linkedlist/DoublyLinkedList.java
+++ Code/List/src/com/vp/ds/linkedlist/DoublyLinkedList.java
@@ -2,30 +2,24 @@ package com.vp.ds.linkedlist;
 
 public class DoublyLinkedList {
 	Node head;
-	Node last;
+	Node tail;
+
 	public void append(int val) {
 		if (head == null) {
 			head = new Node(val);
-			last = head;
+			tail = head;
 		} else {
-			/*
-			Node current = head;
-	
-			while (current.getNext()!= null) {
-				current = current.getNext();
-		
-			}*/
-			Node newNode= new Node(val);
-			newNode.setPrev(last);
-			last.setNext( newNode);
-			last  = newNode ;
+			Node newNode = new Node(val);
+			newNode.setPrev(tail);
+			tail.setNext(newNode);
+			tail = newNode;
 		}
 	}
 
 	public void preppend(int val) {
 		if (head == null) {
 			head = new Node(val);
-			last = head;
+			tail = head;
 		} else {
 
 			Node newNode = new Node(val);
@@ -36,7 +30,39 @@ public class DoublyLinkedList {
 		}
 	}
 
-	public void deleteFirstData(Integer data) {
+
+	public void deleteAllData(Object val) {
+		Node current = head;
+
+		while (current != null) {
+			// 1, 2, 3, 4, 5, 6, 7, 8 9
+			if (current.getData() == val) {
+				if (current.getNext() != null) {
+					if (current.getPrev() != null) {
+						current.getPrev().setNext(current.getNext());
+					} else {
+						// This means head has matching record
+						head = current.getNext();
+					}
+					current.getNext().setPrev(current.getPrev());
+				} else {
+					// Last Node with matching data
+					if (current.getPrev() != null) {
+						current.getPrev().setNext(null);
+					} else {
+						// first node with matching data in case node =1
+						head = null;
+						tail = null;
+					}
+				}
+
+			}
+			current = current.getNext();
+		}
+
+	}
+
+	public void deleteFirst(Integer data) {
 		if (head.getData() == data) {
 			head = head.getNext();
 		} else {
@@ -45,27 +71,50 @@ public class DoublyLinkedList {
 				if (current.getNext().getData() == data) {
 					current.setNext(current.getNext().getNext());
 					break;
-				} 
+				}
 				current = current.getNext();
 			}
 		}
 	}
 
-	public void displayFromFront() {
+	public void display() {
 		Node current = head;
-
+		if (current == null) {
+			System.out.print("List is empty");
+		} else {
 			while (current != null) {
-				System.out.print(current.getData() + ",");
+				System.out.print(current.getData() + "|");
 				current = current.getNext();
 			}
-			System.out.print("\n");
+		}
+
+		System.out.print("\n");
 	}
-	public void displayFromBack() {
-		Node current = last;
+
+	public void displayFromFront() {
+		Node current = head;
+		if (current == null) {
+			System.out.print("List is empty");
+		} else {
 			while (current != null) {
-				System.out.print(current.getData() + ",");
-				current = current.getPrev();
+				System.out.print("Current:" + current.getData());
+				if ((current.getNext() != null))
+					System.out.print(" Next:" + current.getNext().getData());
+				if ((current.getPrev() != null))
+					System.out.print(" Prev:" + current.getPrev().getData());
+				System.out.print("|");
+				current = current.getNext();
 			}
-			System.out.print("\n");
+		}
+		System.out.print("\n");
+	}
+
+	public void displayFromBack() {
+		Node current = tail;
+		while (current != null) {
+			System.out.print(current.getData() + "|");
+			current = current.getPrev();
+		}
+		System.out.print("\n");
 	}
 }
